ebc4ec8c2b65d2e7fb515d7398209da7b7bd6a81
erikfille <erikfilleaudeau@gmail.com>
2023-02-28 15:52:41 -0300

    code refactor

--- api/index.js
+++ api/index.js
@@ -1,11 +1,12 @@
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+require('dotenv').config()
 
 // Syncing all the models at once.
 conn.sync(
   // { force: true }
   ).then(() => {
-  server.listen(3001, () => {
-    console.log('Server is listening at http://localhost:3001'); // eslint-disable-line no-console
+  server.listen(process.env.PORT, () => {
+    console.log('Server is listening at', process.env.PORT); // eslint-disable-line no-console
   });
 });
